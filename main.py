13ef699f9f0894eeb85e4489f8bb35458acbe6fe
Henry Xu <henry.xu98@hotmail.com>
2021-12-01 12:30:48 -0800

    remove token

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
   
-client.run("OTE1Njg4MTE3MzAwNzgxMDY2.YafPAw._h0e_Nxr4UVfMdHYkN1MYoOyME4")
+client.run("token")
